da7efa65692b2cbb77ac228e438269a080d939f4
roshiniemmadi <97740417+roshiniemmadi@users.noreply.github.com>
2022-02-02 13:44:24 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     print(df)
-    print ('Lmabda is working with latest version and creating the new layer version')
+    print ('Lmabda is working with version')
     print('Done x1.1')
